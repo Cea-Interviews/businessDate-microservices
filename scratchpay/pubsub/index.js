1170b525520d2bfaf5ae940cff4102d93ddeda3b
basilcea <ogbonnabasil3@gmail.com>
2020-03-15 18:09:50 +0100

    create server for pubsub service

---
+++ scratchpay/pubsub/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const router = require('./router');
+const logger = require('../logging/logger');
+
+const pubsubServer = express();
+pubsubServer.use(express.json());
+pubsubServer.use(morgan('combined', { stream: logger.stream }));
+pubsubServer.use('/api/v1/subscriptions', router);
+const port = 9000;
+pubsubServer.listen(port, () => {
+  logger.warn(`pub sub service running on port ${port} ...`);
+});
+
+module.exports = pubsubServer;
---
+++ scratchpay/pubsub/tests/index.spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const chai = require('chai');
+const server = require('../index');
+
+const { expect } = chai;
+describe('pub sub server.js', () => {
+  it('should return server listening', async () => {
+    const res = await server.listen(5555);
+    expect(res.listening).to.equal(true);
+  });
+});
