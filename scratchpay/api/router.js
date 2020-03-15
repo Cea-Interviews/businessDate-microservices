1ec3337f3d5d2191a6c7b7c2eea0adc3a7e2c18d
basilcea <ogbonnabasil3@gmail.com>
2020-03-15 19:01:05 +0100

    initiliazed master

--- scratchpay/api/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const controllers = require('./controller/controller');
-
-const router = express.Router();
-router.get('/getBusinessDateWithDelay', controllers.businessDate);
-router.post('/getBusinessDateWithDelay', controllers.businessDate);
-router.get('/isBusinessDay', controllers.checkBusinessDay);
-module.exports = router;
