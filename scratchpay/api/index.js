9eabd53d2fadd76cf68ae608c1188e271100c8af
basilcea <ogbonnabasil3@gmail.com>
2020-03-15 16:32:14 +0100

    write simple tests for generateCalendar

--- scratchpay/api/index.js
+++ scratchpay/api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const apiServer = express()
-
+apiServer.use(express.json())
 apiServer.listen (8000, () => {
     console.log("Business date api service running on port 8000")
 })
